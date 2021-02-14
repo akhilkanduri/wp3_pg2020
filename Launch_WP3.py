3b48c258ac97848f0e2e60acc0106c69fd1ae9e5
Akhil Kanduri <73551215+akhilkanduri@users.noreply.github.com>
2021-02-14 23:49:03 +0100

    Update Launch_WP3.py

--- Launch_WP3.py
+++ Launch_WP3.py
@@ -16,4 +16,4 @@ A = pymsgbox.alert("Launch Frenet Planner ? ",title="WP3 Pkg Invoked")
 
 if A=='OK':
     print('Python File invoked...')
-    os.system("roslaunch carla_autoware_agent carla_autoware_agent.launch town:=Town03 use_ground_truth_localization:=true use_ground_truth_detection:=true spawn_point:=-74,86,2,0,0,90 & python wp3_pg2020/Ak_Test.py &")    
+    os.system("roslaunch carla_autoware_agent carla_autoware_agent.launch town:=Town03 use_ground_truth_localization:=true use_ground_truth_detection:=true spawn_point:=-74,86,2,0,0,90 & python Ak_Test.py &")    
