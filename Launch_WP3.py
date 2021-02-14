d2ec77369f73f4f2bbfe540457dd43dcc78af2f4
Akhil Kanduri <73551215+akhilkanduri@users.noreply.github.com>
2021-02-15 00:20:47 +0100

    Update Launch_WP3.py

--- Launch_WP3.py
+++ Launch_WP3.py
@@ -1,19 +1,13 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Feb 14 16:49:25 2021
-
-@author: lfzt3
-"""
-
 import roslaunch
 import rospy
 import os
 import pymsgbox
 import matplotlib.pyplot as plt
+import thread
 
+thread.start_new_thread(os.system, ('roslaunch carla_autoware_agent carla_autoware_agent.launch town:=Town03 use_ground_truth_localization:=true use_ground_truth_detection:=true spawn_point:=-74,86,2,0,0,90',))
 A = pymsgbox.alert("Launch Frenet Planner ? ",title="WP3 Pkg Invoked")
-
 if A=='OK':
     print('Python File invoked...')
-    os.system("roslaunch carla_autoware_agent carla_autoware_agent.launch town:=Town03 use_ground_truth_localization:=true use_ground_truth_detection:=true spawn_point:=-74,86,2,0,0,90 & python Ak_Test.py &")    
+    thread.start_new_thread(os.system, ('python wp3_test/trail_launch.py',))
+
